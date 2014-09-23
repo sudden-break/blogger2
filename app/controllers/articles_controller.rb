9718049afcdfab8359e131989dc981993e6fbcf5
Andreas Hubenthal <ibu2002@gmail.com>
2014-09-23 13:14:20 +0000

    Add new and create action plus forms

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,22 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
     
     def index
-       @articles = Article.all 
+        @articles = Article.all 
     end
     
     def show
-       @article = Article.find(params[:id]) 
+        @article = Article.find(params[:id]) 
+    end
+    
+    def new
+        @article = Article.new
+    end
+    
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        
+        redirect_to articles_path(@article)
     end
 end
