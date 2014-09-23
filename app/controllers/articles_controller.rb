75bd2b4a4bffdb7e2e260ead49e453270bcad3a5
Andreas Hubenthal <ibu2002@gmail.com>
2014-09-23 13:42:36 +0000

    Add flash notice and change root route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,8 @@ class ArticlesController < ApplicationController
         @article = Article.new(article_params)
         @article.save
         
+        flash.notice = "Article '#{@article.title}' created!"
+        
         redirect_to article_path(@article)
     end
     
@@ -28,12 +30,16 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
         @article.update(article_params)
         
+        flash.notice = "Article '#{@article.title}' updated!"
+        
         redirect_to article_path(@article)
     end
     
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
+        
+        flash.notice = "Article '#{@article.title}' deleted!"
         
         redirect_to articles_path
     end
