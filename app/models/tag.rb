8d0ca40ef18325313c0c31c1bc13660985a744f0
Andreas Hubenthal <ibu2002@gmail.com>
2014-09-24 07:52:22 +0000

    Add Tags

---
+++ app/models/tag.rb
@@ -0,0 +1,10 @@
+class Tag < ActiveRecord::Base
+    has_many :taggings
+    has_many :articles, through: :taggings
+    
+    def tag_list
+        self.tags.collect do |tag|
+            tag.name
+        end.join(", ") 
+    end
+end
